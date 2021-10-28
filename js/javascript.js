9ce04a369847f70297316393f71b584b5b8a9ca3
Shehab Ibrahem <shehabibrahem222@gmail.com>
2021-10-28 13:26:25 -0700

    Initial commit

---
+++ js/javascript.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  /* Dropdown -one*/
+
+  $(".dropdown-one").on("mouseover", function () {
+    $(".dropdown-menu-one").slideDown(200);
+  });
+  $(".dropdown-one").on("mouseleave", function () {
+    $(".dropdown-menu-one").slideUp(200);
+  });
+  /* Dropdown-Tow */
+  $(".dropdown-menu-tow").hide();
+  $(".dropdown-tow").on("mouseover", function () {
+    $(".dropdown-menu-tow").slideDown(200);
+  });
+  $(".dropdown-tow").on("mouseleave", function () {
+    $(".dropdown-menu-tow").slideUp(200);
+  });
+  //   /* Start slideToggle */
+  $("span.navbar-toggler-icon").click(function () {
+    $(".collapse").slideToggle();
+  });
+});
+
+/*  End slideToggle */
